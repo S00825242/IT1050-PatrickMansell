07b95453e1d669d7e9234b7c673b5e904ca51c84
Sloth <Sloth@DESKTOP-EFRAL6H>
2019-11-11 20:27:45 -0500

    adding for Lab3

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* the only difference between an if and then statement is that the if statement will be read one time at the most. While statements will continue to be read until the loop condition defaults to a false.*/
+
+            int speedlimit = 35;
+
+            int speed = 42;
+
+            if (speed > speedlimit)
+
+            { Console.WriteLine("SLOW NOW"); }
+
+            int grade = 87;
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("You get an A");
+            }
+            else if (grade >= 80) ;
+
+            {
+                Console.WriteLine("You get a B");
+
+
+                bool ifTrue; // True if the input data is valid
+                bool ifFalse; // True if the process is false
+
+
+
+                int Celsius = 100;
+
+
+                double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            Codetoconvert: Celsius = (fahrenheit - 32d) * 5d / 9d;
+
+                int i = 0;
+                while (i < 10) ;
+                {
+                    Console.WriteLine("Value of i: {0}", i);
+
+                    i++;
+                }
+
+
+
+            }
+            int p = 60;
+            while (p > 20) ;
+            { Console.WriteLine("Value of p: {5}", p); }
+
+            int g = 10;
+            while (g > 20) ;
+            { Console.WriteLine("Value of g: {2}, g"); }
+
+        }
+
+
+    }
+}
+ 
+
+
